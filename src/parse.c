d4594da8784c4a6c94f9a4125b768844c5305c28
Nikolai Kliuev <kluyev.nikolay@gmail.com>
2025-10-06 19:48:30 +0100

    adding and reading employees fixes

--- src/parse.c
+++ src/parse.c
@@ -56,7 +56,7 @@ void list_employees(struct dbheader_t* dbhdr, struct employee_t* employees)
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring)
 {
-    if (employees == NULL)
+    if (*employees == NULL)
     {
         printf("Null pointer passed a an employees list\n");
         return STATUS_ERROR;
